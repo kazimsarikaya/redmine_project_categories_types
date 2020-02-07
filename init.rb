c23a115520c26949c426456590be844502718754
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2020-02-07 11:53:54 +0300

    first commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :customfield_enumeration do
+  name 'Customfield Enumeration plugin'
+  author 'Kazım SARIKAYA'
+  description 'This is a enumation custom field plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/kazimsarikaya/redmine_customfield_enumeration'
+  author_url 'https://srkykzm.com/about'
+end
